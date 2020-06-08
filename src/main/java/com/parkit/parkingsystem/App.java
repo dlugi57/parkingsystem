794f12cde5c43cf1f4b9396538b9a5e331a22683
dlugi57 <dlugoszpeter@gmail.com>
2020-06-09 01:25:04 +0200

    first

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -3,7 +3,7 @@ package com.parkit.parkingsystem;
 import com.parkit.parkingsystem.service.InteractiveShell;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+//test
 public class App {
     private static final Logger logger = LogManager.getLogger("App");
     public static void main(String args[]){
